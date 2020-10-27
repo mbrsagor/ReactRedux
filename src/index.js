fca18be0ca40214f0b4b3b55b5f2f3929ebaaeb5
Sagor <mbrsagor@gmail.com>
2020-10-28 00:07:43 +0600

    Refactor UI

--- src/index.js
+++ src/index.js
@@ -2,14 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import { BrowserRouter as Router } from 'react-router-dom';
 
 
-ReactDOM.render(
-  <Router>
-    <App />
-  </Router>,
-  document.getElementById('root')
-);
+ReactDOM.render(<App />,document.getElementById('root'));
 
 serviceWorker.unregister();
