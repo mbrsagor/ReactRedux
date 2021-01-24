c191d990577b0b6680600a8d593f672e91b33402
Sagor <mbrsagor@gmail.com>
2021-01-24 23:12:53 +0600

    Post list UI beautification

--- src/pages/Post.jsx
+++ src/pages/Post.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
+import { Link } from "react-router-dom";
 import PostHeader from "../components/PageHader";
 import { fetchPost } from "../store/actions/postAction";
 
@@ -10,17 +11,25 @@ class Post extends Component {
 
   render() {
     let { posts } = this.props;
-    console.log(posts.results);
+    // console.log(posts.results);
     return (
       <div>
         <PostHeader currentPageName="Post Page" />
+        <div className="row mt-2">
+          <div className="col-md-3"></div>
+          <div className="col-md-9 text-right">
+            <Link className="btn btn-success btn-sm" to="/">Add New Post</Link>
+          </div>
+        </div>
         <div className="row">
-          <table className="table table-striped table-border mt-5">
+          <table className="table table-striped table-border mt-3">
             <thead>
               <tr>
                 <th>Post title</th>
-                <th>Category Name</th>
-                <th>Date</th>
+                <th>Author</th>
+                <th>Category</th>
+                <th>Publish Date</th>
+                <th>Actions</th>
               </tr>
             </thead>
             <tbody>
@@ -31,8 +40,22 @@ class Post extends Component {
                       <td>
                         <p>{post.title}</p>
                       </td>
+                      <td>
+                        <p>{post.author.username}</p>
+                      </td>
                       <td>{post.post_category.name}</td>
                       <td>{post.created_at}</td>
+                      <td>
+                        <Link className="btn btn-info btn-sm mr-2" to="/">
+                          Details
+                        </Link>
+                        <button className="btn btn-success btn-sm mr-2">
+                          Update
+                        </button>
+                        <button className="btn btn-danger btn-sm mr-2">
+                          Delete
+                        </button>
+                      </td>
                     </tr>
                   );
                 })}
