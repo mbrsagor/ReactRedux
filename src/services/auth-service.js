0b958260b196bf1034b52eb25020130d86b6ebeb
mbrsagor <mbrsagor@gmail.com>
2022-11-19 20:33:21 +0600

    New: add auth service

---
+++ src/services/auth-service.js
@@ -0,0 +1,23 @@
+// auth-service.js
+export const signUp = (body) => {
+  return fetch({
+    method: 'POST',
+    url: `${API}/sign-up`,
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(body),
+  });
+};
+
+export const signIn = (body) => {
+  return fetch({
+    method: 'POST',
+    url: `${API}/sign-in`,
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(body),
+  });
+};
+
